9b7e1289c3a3d21df92952b6260a69ea5a71af40
Pavani89 <32454769+Pavani89@users.noreply.github.com>
2022-04-23 21:46:02 +0530

    Update anagrams.py
    final code

--- anagrams.py
+++ anagrams.py
@@ -1,7 +1,7 @@
 '''
 Problem statement:
 Given two strings, check to see if they are anagrams.
-eg: 
+eg:
 "client eastwood" is an anagram of "old west action"
 '''
 
@@ -18,37 +18,39 @@ def anagram(s1, s2):
 anagram('dog','god')
 '''
 
-#solution 2:
+
+# solution 2:
 
 def anagram(s1, s2):
-  #remove spaces and lower
-  s1= s1.replace(" ", "").lower()
-  s2= s2.replace(" ", "").lower()
-  
-  #check if the same number of letters
-  if len(s1) != len(s2):
-    return False
-  
-  # count the frequncy of each letter
-  count = {}
-  
-  for letter in s1:
-    if letter in count:
-      count[letter] += 1  #add 1 to the letter if it exits in the dictionary
-    else:
-      count[letter] = 1
-      
-    for letter in s2:
-    if letter in count:
-      count[letter] -= 1  #add 1 to the letter if it exits in the dictionary
-    else:
-      count[letter] = 1 
-      
-    for k in count:
-      if count[k] != 0 
+    # remove spaces and lower
+    s1 = s1.replace(" ", "").lower()
+    s2 = s2.replace(" ", "").lower()
+
+    # check if the same number of letters
+    if len(s1) != len(s2):
         return False
-    
-    return True
-  
- x = anagram('god','dog')
-print x
+
+    # count the frequency of each letter
+    count = {}
+
+    for letter in s1:
+        if letter in count:
+            count[letter] += 1  # add 1 to the letter if it exits in the dictionary
+        else:
+            count[letter] = 1
+
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1  # add 1 to the letter if it exits in the dictionary
+        else:
+            count[letter] = 1
+
+    for k in count:
+        if count[k] != 0:
+            return False
+
+        return True
+
+
+x = anagram('god', 'dog')
+print(x)
