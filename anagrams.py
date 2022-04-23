3b46a95261e1844871b2bd766f4bf14983e3ef95
Pavani89 <32454769+Pavani89@users.noreply.github.com>
2022-04-23 20:39:51 +0530

    Update anagrams.py
    completed code

--- anagrams.py
+++ anagrams.py
@@ -25,4 +25,30 @@ def anagram(s1, s2):
   s1= s1.replace(" ", "").lower()
   s2= s2.replace(" ", "").lower()
   
+  #check if the same number of letters
+  if len(s1) != len(s2):
+    return False
   
+  # count the frequncy of each letter
+  count = {}
+  
+  for letter in s1:
+    if letter in count:
+      count[letter] += 1  #add 1 to the letter if it exits in the dictionary
+    else:
+      count[letter] = 1
+      
+    for letter in s2:
+    if letter in count:
+      count[letter] -= 1  #add 1 to the letter if it exits in the dictionary
+    else:
+      count[letter] = 1 
+      
+    for k in count:
+      if count[k] != 0 
+        return False
+    
+    return True
+  
+ x = anagram('god','dog')
+print x
