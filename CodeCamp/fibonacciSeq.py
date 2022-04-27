ee2d83b5e7b0f710634ae338830783b94b489216
Pavani89 <32454769+Pavani89@users.noreply.github.com>
2022-04-27 21:07:57 +0530

    updated the fibonacci code to use generators

--- CodeCamp/fibonacciSeq.py
+++ CodeCamp/fibonacciSeq.py
@@ -10,7 +10,7 @@ def fibonacci(n):
        return(fibonacci(n-1) + fibonacci(n-2))
 
 '''
-
+'''
 # Fibonacci sequence using cache or memoization
 def fibonacci(n):
     cache = {0: 0, 1: 1}
@@ -26,3 +26,18 @@ item = 10
 print("Fibonacci sequence:")
 for i in range(item):
     print(fibonacci(i))
+'''
+
+
+# Fibonacci generator using py genertor code
+
+def fibonacci(num):
+    a, b = 0, 1
+    for i in range(0, num):
+        yield "{}: {}".format(i + 1,
+                              a)  # Use generators to display the list; yeild is the keywprd to identify generators
+        a, b = b, a + b
+
+
+for item in fibonacci(10):
+    print(item)
