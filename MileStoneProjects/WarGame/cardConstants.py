75f6f0bc7260f167d9e6ca5577e2c74fd88752bc
Pavani89 <32454769+Pavani89@users.noreply.github.com>
2023-05-13 21:52:22 -0500

    Create cardConstants.py

---
+++ MileStoneProjects/WarGame/cardConstants.py
@@ -0,0 +1,25 @@
+## Global variable
+
+# Card Suits
+suits = ('Hearts', 'Diamond', 'Spades', 'Clubs')
+
+# Card Ranks
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven','Eight',
+          'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+# Value for the cards
+values = {
+    'Two': 2,
+    'Three': 3,
+    'Four': 4,
+    'Five': 5,
+    'Six': 6,
+    'Seven': 7,
+    'Eight': 8,
+    'Nine': 9,
+    'Ten': 10,
+    'Jack': 11,
+    'Queen': 12,
+    'King': 13,
+    'Ace': 14
+}
